7172341d4af729a217105848360df8f1bab2a05f
EricFormar <66625345+EricFormar@users.noreply.github.com>
2021-02-25 11:45:09 -0300

    añade crear pelicula

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,29 +1,83 @@
 const db = require('../database/models');
+const {Op} = require('sequelize');
+
+const moment = require('moment');
 
 module.exports = {
     index : (req,res) => {
         res.render('movies')
     },
     list : (req,res) => {
-        db.Pelicula.findAll()
-        .then(peliculas => {
+        let peliculas = db.Pelicula.findAll({
+            order : [
+                ['title', 'ASC']
+            ]
+        })
+        let cantidad = db.Pelicula.count();
+
+        let top = db.Pelicula.findAll({
+            order : [
+                ['rating','DESC']
+            ],
+            limit : 10
+        })
+
+        Promise.all([peliculas,cantidad,top])
+        .then(([peliculas,cantidad,top]) => {
             return res.render('moviesList', {
-                peliculas
+                peliculas,
+                cantidad,
+                top
+            })
+        })
+        .catch(error => console.log(error))
+    },
+    show : (req,res) => {
+        db.Pelicula.findByPk(req.params.id)
+        .then(pelicula => {
+            return res.render('moviesShow',{
+                pelicula,
+                estreno : moment(pelicula.release_date).format('d MMMM YYYY')
+            })
+        })
+        .catch(error => res.send(error))
+
+    },
+    create : (req,res) => {
+        db.Genero.findAll()
+        .then(generos => {
+            return res.render('moviesAdd',{
+                generos
             })
         })
         .catch(error => res.send(error))
     },
-    show : (req,res) => {
-
-    },
-    create : (req,res) => {
-
-    },
     store : (req,res) => {
-
+        const {title, rating, awards, release_date, length, genre_id} = req.body;
+        db.Pelicula.create({
+            title : title.trim(),
+            rating : +rating,
+            awards: +awards,
+            release_date,
+            length: +length,
+            genre_id : +genre_id
+        })
+        .then(() => {
+            return res.redirect('/movies/list')
+        })
+        .catch(error => res.send(error))
     },
     edit : (req,res) => {
-
+        let pelicula = db.Pelicula.findByPk(req.params.id)
+        let generos = db.Genero.findAll()
+        Promise.all([pelicula,generos])
+        .then(([pelicula, generos])=>{
+            return res.render('moviesEdit',{
+                pelicula,
+                generos
+            })
+        })
+        .catch(error => res.send(error))
     },
     update : (req,res) => {
 
@@ -32,6 +86,18 @@ module.exports = {
 
     },
     search : (req,res) => {
-
+       db.Pelicula.findAll({
+           where : {
+               title : {
+                   [Op.like] : `%${req.query.busqueda}%`
+               }
+           }
+       })
+       .then(peliculas => {
+           return res.render('moviesResult',{
+               peliculas
+           })
+       })
+       .catch(error => res.send(error))
     }
 }
--- routes/movies.js
+++ routes/movies.js
@@ -4,7 +4,13 @@ const router = express.Router();
 const {index, list, show, create, store, edit, update,remove,search} = require('../controllers/moviesController')
 
 router.get('/index',index);
-router.get('/list',list)
+router.get('/list',list);
+router.get('/show/:id',show);
+router.get('/search',search);
+router.get('/create',create);
+router.post('/create',store);
+router.get('/edit/:id',edit);
+router.put('/edit/:id',update);
 
 
 module.exports = router;
